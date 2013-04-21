6c1017ac90ad0695f14d8f67877f02d474c8b207
Tobias Domhan <tdomhan@gmail.com>
2013-04-21 17:19:48 -0400

    plotting of weights

--- ActivityRecognitionFromSmartphoneData/src/pycrf/crf.py
+++ ActivityRecognitionFromSmartphoneData/src/pycrf/crf.py
@@ -19,12 +19,12 @@ import time
 
 
 def predict_test_words(test_imgs,test_words,fweights,tweights):
-    #print process_test_word_mp(test_imgs[i], test_words[i], fweights, tweights, debug = True)
+    #print process_labels_mp(test_imgs[i], fweights, tweights, debug = True)
 
     #for i in range(0,5):     
     #    print "case %d" % (i+1)  
     #    print "         word: %s" % id2char(test_words[i])
-    #    pword, _ = process_test_word_mp(test_imgs[i], test_words[i], fweights, tweights, debug = False)
+    #    pword, _ = process_labels_mp(test_imgs[i], fweights, tweights, debug = False)
     #    print "predicted word %s" % id2char(pword)
     
     num_characters = 0
@@ -32,8 +32,9 @@ def predict_test_words(test_imgs,test_words,fweights,tweights):
     for i in range(0,len(test_words)):     
         #print "case %d" % (i+1)  
         #print "         word: %s" % id2char(test_words[i])
-        pword, _ = process_test_word_mp(test_imgs[i], test_words[i], fweights, tweights)
-        print pword[0:100]
+        beta = process_labels_mp(test_imgs[i], fweights, tweights)
+        pword = predict_labels(beta)
+        #print pword[0:200]
         
         #print "predicted word %s" % id2char(pword)
         for j,c in enumerate(pword):
@@ -71,7 +72,7 @@ def get_neg_transition_energy(labels, phi_trans):
     return neg_engergy
 
 
-def process_test_word_mp(img,txt,fweights,tweights):
+def process_labels_mp(img,fweights,tweights):
     """
     Sum-Product Message Passing
     """
@@ -130,17 +131,21 @@ def process_test_word_mp(img,txt,fweights,tweights):
     deltab.reverse()
     for i,d in enumerate(deltab):
         beta[i] = beta[i] + d
-        
+    
+    return beta
+
+
+def predict_labels(beta):
+    """
+    Predict a labels sequence using the betas
+    calculate in from the message passing algorithm.
+    """
     pword = []
     for b in beta:
-        #pword.append(np.argmax(np.exp(logsumexp(b, axis=1))))
         pword.append(np.argmax(logsumexp(b, axis=1)))
-    #print "probabilities:"
-    #print np.exp(logsumexp(b, axis=1))
-    #pword.append(np.argmax(np.exp(logsumexp(beta[-1], axis=0))))
     pword.append(np.argmax(logsumexp(beta[-1], axis=0)))
     
-    return pword, beta
+    return pword
 
 def get_neg_energ(labels,phi_ij,phi_trans):
     """
@@ -186,7 +191,7 @@ class CRFTrainer():
         for img, word in zip(train_imgs, train_words):
             #print "BEGIN WORD: %f" % (time.clock() - tick)
             tick = time.clock()
-            _, beta = process_test_word_mp(img, word, lfweights, ltweights)
+            beta = process_labels_mp(img, lfweights, ltweights)
             Z = logsumexp(beta[0])
             phi_ij = get_conditioned_weights(img, lfweights)
             
@@ -235,9 +240,9 @@ class CRFTrainer():
                 if j < len(beta):
                     b = beta[j]
                     P_ccn = np.exp(b-Z)
-#                    
-##                tderivatives[y_ij][y_ij_n] += 1
-##                tderivatives += -P_ccn
+                    
+                tderivatives[y_ij][y_ij_n] += 1
+                tderivatives += -P_ccn
                 
                 #unvectorized code for reference
 #                for c in range(0,self.n_labels):
@@ -279,9 +284,9 @@ class CRFTrainer():
         return reg
         
     def train(self):
-        res = minimize(self.crf_log_lik, np.zeros((self.n_fweights+self.n_tweights,1)), args = (self.Xs, self.ys_labels), method='BFGS', jac=True, options={'disp': True}, callback=self.test_accuracy)
+        res = minimize(self.crf_log_lik, np.zeros((self.n_fweights+self.n_tweights,1)), args = (self.Xs, self.ys_labels), method='BFGS', jac=True, options={'disp': True, 'maxiter': 100}, callback=self.test_accuracy)
         
-        self.fweights = res.x[0:self.n_fweights].reshape(self.n_labels,self.n_fweights)
+        self.fweights = res.x[0:self.n_fweights].reshape(self.n_labels,self.n_features)
         self.tweights = res.x[self.n_fweights:].reshape(self.n_labels,self.n_labels)
         
     def get_weights(self):
@@ -290,25 +295,31 @@ class CRFTrainer():
     def test_accuracy(self, xk):
         if self.Xs_test == None or self.ys_test_labels == None:
             return
+        
         learnedfweights = xk[0:self.n_fweights].reshape(self.n_labels,self.n_features)
         learnedtweights = xk[self.n_fweights:].reshape(self.n_labels,self.n_labels)
         
-        #print learnedtweights
-        #print learnedfweights
+        #TODO: don't store the temporary learned weights
+        self.fweights = learnedfweights
+        self.tweights = learnedtweights
         
         predict_test_words(self.Xs_test,self.ys_test_labels,learnedfweights,learnedtweights)
 
 class LinearCRF(BaseEstimator):
     
-    def __init__(self):
+    def __init__(self, sigma=None):
+        """
+            sigma: L2 regularization parameter
+        """
         self.label_names = np.array([])
         self.labels = np.array([])
+        self.sigma = sigma
     
     def set_params(self, **parameters):
         #TODO: implement!
         pass
     
-    def fit(self, X, y, X_test=None, y_test=None, sigma=None):
+    def fit(self, X, y, X_test=None, y_test=None):
         """Fit the CRF model (for a single chain) according to the given training data.
 
         Parameters
@@ -344,11 +355,11 @@ class LinearCRF(BaseEstimator):
         ys = [y]
         Xs_test = [X_test] if X_test != None else None
         ys_test = [y_test] if y_test != None else None
-        self.batch_fit(Xs, ys, Xs_test, ys_test, sigma)
+        self.batch_fit(Xs, ys, Xs_test, ys_test)
         
         return self
     
-    def batch_fit(self, Xs, ys, Xs_test=None, ys_test=None, sigma=None):
+    def batch_fit(self, Xs, ys, Xs_test=None, ys_test=None):
         """Fit the CRF model according to the given training data.
 
         Parameters
@@ -359,8 +370,6 @@ class LinearCRF(BaseEstimator):
 
         y : iterable of array-like, shape = [n_samples]
             Target values (class labels)
-            
-        sigma: L2 regularization parameter
         
         Xs_test : iterable of {array-like, sparse matrix}, shape = [n_samples, n_features]
             Test vectors, where n_samples is the number of samples
@@ -383,23 +392,32 @@ class LinearCRF(BaseEstimator):
         n_features = Xs[0].shape[1]
         
         self.label_names, _ = np.unique(np.concatenate(ys), return_inverse=True)
-        label_mapper = defaultdict(lambda :-1)
-        label_mapper.update({label:i for i,label in enumerate(self.label_names)})
+        self.label_mapper = defaultdict(lambda :-1)
+        self.label_mapper.update({label:i for i,label in enumerate(self.label_names)})
+        self.inv_label_mapper = defaultdict(lambda :-1)
+        self.inv_label_mapper.update({i:label for i,label in enumerate(self.label_names)})
         
-        ys_labels = [np.array([label_mapper[i] for i in y]) for y in ys]
+        ys_labels = [np.array([self.label_mapper[i] for i in y]) for y in ys]
         ys_test_labels = None
         if ys_test:
-            ys_test_labels = [np.array([label_mapper[i] for i in y]) for y in ys_test]
+            ys_test_labels = [np.array([self.label_mapper[i] for i in y]) for y in ys_test]
 
         n_labels = len(self.label_names)
         
-        trainer = CRFTrainer(Xs, ys_labels, Xs_test, ys_test_labels, n_labels, n_features, sigma)
+        self.trainer = CRFTrainer(Xs, ys_labels, Xs_test, ys_test_labels, n_labels, n_features, self.sigma)
         
-        trainer.train()
+        self.trainer.train()
         
-        self.fweights, self.tweights = trainer.get_weights()
+        self.fweights, self.tweights = self.trainer.get_weights()
+        del self.trainer
         
         return self
+    
+    def predict(self, X):
+        beta = process_labels_mp(X, self.fweights, self.tweights)
+        labels = np.array(predict_labels(beta))
+        labels = np.array([self.inv_label_mapper[l] for l in labels])
+        return labels
     
     def predict_batch(self, Xs):
         """Perform inference on samples in X.
@@ -413,4 +431,37 @@ class LinearCRF(BaseEstimator):
         -------
         y_pred : array, shape = [n_samples]
         """
-        pass
+        results = []
+        for X in Xs:
+            results.append(self.predict(X))
+
+        return results
+    
+    
+    def plot_weights(self):
+        """
+        Plot the weight matrices using matplotlib.
+        """
+        
+        import matplotlib.pyplot as plt
+        import matplotlib.cm as cm
+
+        n_labels = len(self.label_names)
+                
+        plt.figure(1)
+        for i in range(n_labels):
+            #TODO: generalize
+            plt.subplot(2,5,i+1);
+            plt.imshow(np.reshape(self.fweights[i,1:],(16,20)).T,interpolation='nearest',cmap = cm.Greys_r);
+            #plt.title(labels[i]);
+            plt.colorbar(shrink=0.6)
+            plt.xticks([])
+            plt.yticks([])
+        
+        plt.figure(2)
+        plt.imshow(self.tweights,interpolation='nearest',cmap = cm.Greys_r);
+        #plt.xticks(range(10),labels)
+        #plt.yticks(range(10),labels)
+        plt.colorbar();
+        
+        plt.show()
